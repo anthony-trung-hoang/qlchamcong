41128169df6fa836aa13b8062c8347cbc87f2a2a
Hoàng Quốc Trung <hoangquoctrung02@hotmail.com>
2023-12-13 10:43:19 +0700

    change version

--- src/main/java/com/example/qlchamcong/qlnshome/QLNSHomeViewManager.java
+++ src/main/java/com/example/qlchamcong/qlnshome/QLNSHomeViewManager.java
@@ -2,6 +2,7 @@ package com.example.qlchamcong.qlnshome;
 
 import com.example.qlchamcong.HelloApplication;
 import com.example.qlchamcong.entity.WorkerAttendanceData;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,11 +10,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -23,35 +27,16 @@ public class QLNSHomeViewManager implements Initializable {
     @FXML
     private Label homePageText;
 
-    @FXML
-    private Button suaBGCC;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         qlnsHomeController = new QLNSHomeController();
         fetchAndDisplayTableData();
     }
 
-    @FXML
-    protected void handleSuaBGCCAction() {
-        Parent root;
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/qlchamcong/dangnhap/dang-nhap.fxml"));
-            root = fxmlLoader.load();
-            Stage stage = new Stage();
-
-            stage.setTitle("My New Stage Title");
-            stage.setScene(new Scene(root));
-            stage.show();
-
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void fetchAndDisplayTableData () {
+    public void fetchAndDisplayTableData() {
         List<WorkerAttendanceData> workerAttendanceDataList = qlnsHomeController.fetchTableData();
+
+
     }
 }
